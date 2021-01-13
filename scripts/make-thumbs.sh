81f657fbd884fd28f19af412c6fe0d8a17139d1d
Roman Nurik <roman@nurik.net>
2021-01-13 00:09:41 -0500

    Script updates

--- scripts/make-thumbs.sh
+++ scripts/make-thumbs.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-
-# TODO:
-# video conversion / format unification
-# https://gist.github.com/Vestride/278e13915894821e1d6f
-
 # dependencies:
 # - ffmpeg
 # - optipng
@@ -12,7 +7,12 @@ for f in app/**/*.mp4; do
   base=${f%.mp4}
   thumb="${base}-thumb.png"
   tempfile="${base}-thumb.tmp.png"
-  ffmpeg -y -i "$f" -ss 00:00:00.01  -vframes 1 "$tempfile"
+  ffmpeg \
+      -y \
+      -i "$f" \
+      -ss 00:00:00.01 \
+      -vframes 1 \
+      "$tempfile"
   optipng "$tempfile"
   pngquant --force "$tempfile" --output "$thumb"
   rm "$tempfile"
---
+++ scripts/make-videos.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# https://gist.github.com/Vestride/278e13915894821e1d6f
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+while [[ "$1" != "" ]]; do
+  in="$1"
+  base=$(basename "$1")
+  base=${base/.*/}
+
+  ffmpeg \
+      -y -an \
+      -i "$in" \
+      -vcodec h264 \
+      -r 60 \
+      -pix_fmt yuv420p -profile:v baseline -level 3 -strict -2 \
+      "${SCRIPT_DIR}/../app/media/${base}.mp4"
+
+  #echo $1
+  shift
+done
