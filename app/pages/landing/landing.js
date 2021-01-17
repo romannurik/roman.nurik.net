be554720545201fce658a17bebb71d4b49ebd85b
Roman Nurik <roman@nurik.net>
2021-01-17 00:42:52 -0500

    Auto dark theme, minor type fixes, body height

--- app/pages/landing/landing.js
+++ app/pages/landing/landing.js
@@ -1,6 +1,10 @@
 import './landing.scss';
 
 export function LandingPage() {
+  let update = () => document.body.style.setProperty('--vh', `${window.innerHeight / 100}px`);
+  update();
+  window.addEventListener('resize', update, false);
+
   window.addEventListener('load', () => {
     for (let link of document.querySelectorAll('.obfuscated-email')) {
       let email = link.getAttribute('data-email');
