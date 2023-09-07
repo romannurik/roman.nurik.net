05b02cbccc5ce27cc6aa05422d350aec31ab2636
Roman Nurik <roman@nurik.net>
2023-09-07 00:25:56 -0400

    fixes for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import yaml from '@modyfi/vite-plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://roman.nurik.net',
+  build: {
+    assets: 'astro', // underscores don't work in GitHub Pages
+  },
   vite: {
     plugins: [
       yaml()
